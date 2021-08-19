397989109c1fcf18890885a90af93e32cd7247a6
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-19 13:13:23 +0530

    changed names to database and errorlog

--- R/write_csvs.R
+++ R/write_csvs.R
@@ -1,6 +1,6 @@
-write_csvs <- function(spreadsheet,spreadsheet_error){
-  write.table(spreadsheet,file='nlsDatabase.csv',append=TRUE,
+write_csvs <- function(database,errorlog){
+  write.table(database,file='nlsDatabase.csv',append=TRUE,
               sep=",",col.names=FALSE,row.names=FALSE) #?? where to include write.table
-  write.table(spreadsheet_error,file='nlsErrorLog.csv',append=TRUE,
+  write.table(errorlog,file='nlsErrorLog.csv',append=TRUE,
               sep=",",col.names=FALSE,row.names=FALSE)
 }
