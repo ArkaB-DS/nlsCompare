ad9cc6450c7347e59d9f354f972fb7178004f0ef
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-19 20:20:41 +0530

    changes in create_elog() for same reason as for create_db()

--- R/create_elog.R
+++ R/create_elog.R
@@ -1,5 +1,7 @@
 create_elog<- function(){
-  errorlog <- data.frame(DateTime="",MachID="",FileName="",
-				 Solver="",Algorithm="",Control="",Message="")
+  column_names <- c("DateTime","MachID","FileName",
+                    "Solver","Algorithm","Control","Message")
+  errorlog <- data.frame(matrix(nrow = 0, ncol = length(column_names)))
+  colnames(errorlog) <- column_names
   return(errorlog)
 }
