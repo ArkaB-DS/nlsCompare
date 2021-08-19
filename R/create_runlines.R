c35aa79630ba82638dfc214eff84950ee92025cf
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-19 23:08:44 +0530

    minor update

--- R/create_runlines.R
+++ R/create_runlines.R
@@ -3,34 +3,36 @@ create_runline<-function(NLSsolver,NLSalgorithm,NLScontrol,NLSdata,
 			NLSna.action=NULL,NLSmasked=NULL){
 
 	NLSrunline <- "(formula=NLSformula,data=NLSdata,
-				      start=NLSstart,
-					lower=NLSlower,
-					upper=NLSupper,
-					weights=NLSweights,
-					subset=NLSsubset,
-					#na.action=NLSna.action,
-					algorithm=NLSalgorithm,
-					control=NLScontrol)"
+				          start=NLSstart,
+        					lower=NLSlower,
+        					upper=NLSupper,
+        					weights=NLSweights,
+        					subset=NLSsubset,
+        					#na.action=NLSna.action,
+        					algorithm=NLSalgorithm,
+        					control=NLScontrol)"
+
 	if (NLScontrol=="nlsr::nlxb"){
 		NLSrunline <- "(formula=NLSformula,
-				    data=NLSdata,
-				    start=NLSstart,
-				    lower=NLSlower,
-				    upper=NLSupper,
-				    weights=NLSweights,
-				    #masked=
-      			    control=NLScontrol)"
-		}
+        				    data=NLSdata,
+        				    start=NLSstart,
+        				    lower=NLSlower,
+        				    upper=NLSupper,
+        				    weights=NLSweights,
+        				    #masked=
+              			control=NLScontrol)"
+	    }
+
 	if(NLSsolver=="nlsj::nlsj" & NLSalgorithm=="marquardt"){ #?? SOLVER to be kept?
 		NLSrunline <- "(formula=NLSformula,
-					data=NLSdata,
-					start=NLSstart,
-					lower=NLSlower,
-					upper=NLSupper,
-					weights=NLSweights,
-					subset=NLSsubset,
-					#na.action=
-					control=NLScontrol)"
-		}
+          					data=NLSdata,
+          					start=NLSstart,
+          					lower=NLSlower,
+          					upper=NLSupper,
+          					weights=NLSweights,
+          					subset=NLSsubset,
+          					#na.action=
+          					control=NLScontrol)"
+		  }
 	return(NLSrunline)
 	}
