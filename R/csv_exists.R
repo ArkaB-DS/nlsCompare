91eeb0f52338881c62fbc633f830bffca7e41d1a
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-19 13:04:57 +0530

    correction in function

--- R/csv_exists.R
+++ R/csv_exists.R
@@ -1,7 +1,7 @@
-csv_exists <-function(){
+csv_exists <-function(database,errorlog){
   #rtn<-c(TRUE,TRUE)
   if (!file.exists("nlsDatabase.csv")){
-    write.table(spreadsheet,file='nlsDatabase.csv',append=TRUE,
+    write.table(database,file='nlsDatabase.csv',append=TRUE,
                 sep=",",col.names=TRUE,row.names=FALSE)
     #rtn[1] <- FALSE
     print(" 'nlsDatabase.csv' has been created")
@@ -10,7 +10,7 @@ csv_exists <-function(){
   }
 
   if (!file.exists("nlsErrorLog.csv")){
-    write.table(spreadsheet_error,file='nlsErrorLog.csv',append=TRUE,
+    write.table(errorlog,file='nlsErrorLog.csv',append=TRUE,
                 sep=",",col.names=TRUE,row.names=FALSE)
     #rtn[2] <- FALSE
     print(" 'nlsErrorLog.csv' has been created")
