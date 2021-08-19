a36c991d96807674adfa6b31cca0a8c8ba7ad3dc
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-19 13:45:35 +0530

    updated rm_nls()

--- R/rm_nls.R
+++ R/rm_nls.R
@@ -1,13 +1,8 @@
 rm_nls <- function(){
   #?? how to deal with environment
   rm(Convergence,cpu,Deviance,epstol,Gradient,i,j,machid,
-     NLSdata,NLSformula,NLSlower,NLSmethods,NLSproblems,NLSrunline,
-     NLSstart,NLSsubset,NLSupper,NLSweights,output,output_nls,
-     Parameters,problemNumber,ram,Residuals,Rmat,spreadsheet,
-     sy,scr_dir,spreadsheet_error,ramtry,NLSrunline,errorOtherFlag,
-     errorNumber,errorNLSFlag,checker.mat,checker.resid,checker.pars,
-     checker.rmat.checker.resid,checker.pars,checker.other,checker.nls,
-     checker.grad,checker.dev,checker.conv,checker.rmat,NLSrunline0,
-     NLSssquares,NLSpars,refsol,NLStag,NLSref,
+     NLSdata,NLSformula,NLSlower,NLSmethods,NLSproblems,
+     NLSsubset,NLSupper,NLSweights,
+     NLSssquares,NLSpars,refsol,NLStag,NLSref,NLSrefVersion,
      envir=.GlobalEnv)
 }
