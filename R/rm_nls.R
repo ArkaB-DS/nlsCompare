2bdda350de563d51d2b67ce452ca0f8063b5baa5
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-19 13:56:21 +0530

    further updates in rm_nls

--- R/rm_nls.R
+++ R/rm_nls.R
@@ -1,8 +1,7 @@
 rm_nls <- function(){
   #?? how to deal with environment
-  rm(Convergence,cpu,Deviance,epstol,Gradient,i,j,machid,
-     NLSdata,NLSformula,NLSlower,NLSmethods,NLSproblems,
-     NLSsubset,NLSupper,NLSweights,
+  rm(NLSdata,NLSformula,NLSlower,NLSmethods,NLSproblems,
+     NLSsubset,NLSupper,NLSweights,NLSstart,
      NLSssquares,NLSpars,refsol,NLStag,NLSref,NLSrefVersion,
      envir=.GlobalEnv)
 }
