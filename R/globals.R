2db6d45ee6f6a566460b0fe8e0690be983a2fa9e
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-08-20 19:46:42 +0530

    updates

---
+++ R/globals.R
@@ -0,0 +1,4 @@
+globalVariables(c("NLSdata", "NLSformula", "NLSlower", "NLSmethods",
+                  "NLSpars", "NLSproblems", "NLSref", "NLSrefVersion",
+                  "NLSssquares", "NLSstart", "NLSsubset", "NLStag", "NLSupper",
+                  "NLSweights", "refsol"))
